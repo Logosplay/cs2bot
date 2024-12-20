ae53b251de6677f184a00ee0cc4c61b61d084d19
Nocty04 <a.s.jesus07@gmail.com>
2024-12-20 02:42:07 +0000

    dsad
    dsdadsad

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-
 from selenium_stealth import stealth
 import requests
-import time
+import tim
 
 def main():
     driver = start()
